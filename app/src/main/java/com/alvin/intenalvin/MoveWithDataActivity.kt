da1638b40efae07527d8424eba49a4fba5f2d58a
alvin1998 <32792182+alvin1998@users.noreply.github.com>
2021-11-04 11:35:18 +0700

    Latihan inten
    Alvin Ade Sindy / 20520014

--- app/src/main/java/com/alvin/intenalvin/MoveWithDataActivity.kt
+++ app/src/main/java/com/alvin/intenalvin/MoveWithDataActivity.kt
@@ -1,11 +1,15 @@
 package com.alvin.intenalvin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
-class MoveWithDataActivity : AppCompatActivity() {
+class MoveWithDataActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var tvDataReceved : TextView
+    private lateinit var btnBack1 : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +17,9 @@ class MoveWithDataActivity : AppCompatActivity() {
 
         tvDataReceved = findViewById(R.id.tv_data_receiver)
         DataRecevide()
+
+        btnBack1 = findViewById(R.id.btn_back1)
+        btnBack1.setOnClickListener(this)
 
     }
 
@@ -22,5 +29,16 @@ class MoveWithDataActivity : AppCompatActivity() {
         var alamat = bundle?.getString("Alamat")
 
         tvDataReceved.text = nama + " " + alamat
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+                R.id.btn_back1 -> run {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
